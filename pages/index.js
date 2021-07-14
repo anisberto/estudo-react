7a2cd99ef0559e6b49a19abce98bc802646eb3c2
anisberto Reis <anisbertoos@gmail.com>
2021-07-13 23:31:51 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import { AlurakutMenu } from "../src/lib/AlurakutCommons";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidbar(propriedade) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${propriedade.githubUser}.png`}
+        style={{ borderRadius: "10px" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "anisberto";
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" className={{ gridArea: "profileArea" }}>
+          <ProfileSidbar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" className={{ gridArea: "welcomeArea" }}>
+          <Box>Bem vindo</Box>
+        </div>
+        <div
+          className="profileRelacionsArea"
+          className={{ gridArea: "profileRelacionsArea" }}
+        >
+          <Box>Pessoas da comunidade</Box>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
